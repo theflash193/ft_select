5f1e047166c2ec309016403a8d85e892bd91af62
Gordon RASS-KWASI <grass-kw@e3r13p7.42.fr>
2017-02-24 16:28:53 +0100

    ecriture de la selection dans le tty de sortie

--- resources/src/main/select.c
+++ resources/src/main/select.c
@@ -2,12 +2,14 @@
 
 void	affichage_element(t_select *elem)
 {
+	t_env	*e;
 
+	e = singleton();
 	if (elem->current == 1)
 		underline_on();
 	if (elem->selected == 1)
 		reverse_video();
-	putstr_fd(elem->content, 1);
+	putstr_fd(elem->content, e->tty_out);
 	if (elem->current == 1)
 		underline_off();
 	if (elem->selected == 1)
